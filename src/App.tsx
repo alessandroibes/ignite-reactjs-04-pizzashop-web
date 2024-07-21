41370a191f37b3d51a144149af2592b056f71e18
alessandroibes <alessandro.ibes@gmail.com>
2024-07-21 07:10:04 -0300

    Setup do projeto: Layouts da Aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './global.css'
 
 import { RouterProvider } from 'react-router-dom'
 
-import { router } from './pages/routes'
+import { router } from './routes'
 
 export function App() {
   return <RouterProvider router={router} />
