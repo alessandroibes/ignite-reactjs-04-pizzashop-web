f9fe3980d2c091766733808bcecc37d6bd5e56b9
alessandroibes <alessandro.ibes@gmail.com>
2024-08-01 08:58:13 -0300

    Conexão com API: Produtos populares

--- src/api/get-month-revenue.ts
+++ src/api/get-month-revenue.ts
@@ -1,12 +1,12 @@
 import { api } from '@/lib/axios'
 
-export interface GetMonthRevenueOrdersAmountResponse {
+export interface GetMonthRevenueResponse {
   receipt: number
   diffFromLastMonth: number
 }
 
-export async function getMonthRevenueOrdersAmount() {
-  const response = await api.get<GetMonthRevenueOrdersAmountResponse>(
+export async function getMonthRevenue() {
+  const response = await api.get<GetMonthRevenueResponse>(
     '/metrics/month-receipt',
   )
 
